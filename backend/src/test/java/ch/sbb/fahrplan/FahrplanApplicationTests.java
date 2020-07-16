9fa2c48718db9f7f2ebfa8b7cea3c9d72727d81d
Gerber Philippe Dan (IT-SWE-CCA - Extern) <philippe_dan.gerber@sbb.ch>
2020-07-16 16:27:57 +0200

    Initial commit

---
+++ backend/src/test/java/ch/sbb/fahrplan/FahrplanApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.sbb.fahrplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FahrplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
